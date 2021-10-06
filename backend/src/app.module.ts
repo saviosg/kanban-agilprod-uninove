7622b8cc00ec0ae6a154762c7a6cbf6188cc6310
Sávio SG <25729991+saviosg@users.noreply.github.com>
2021-10-06 16:56:30 -0300

    Add initial integration w/ MikroORM+Postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'agilprod',
+      type: 'postgresql',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
